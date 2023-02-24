b56d6074bfbd643b2c55f69a9cb65f28c3f5afbf
Anton <antonch92@gmail.com>
2023-02-24 21:07:56 +0100

    first commit

---
+++ reactForms1/src/components/converter.jsx
@@ -0,0 +1,6 @@
+const hex2rgb = (c) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(c);
+  return `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[2], 16)})` 
+}
+
+export default hex2rgb;
