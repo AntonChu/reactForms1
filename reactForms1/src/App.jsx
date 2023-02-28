274c6895618f9c35170af572d39a07a8ecd4489d
Anton <antonch92@gmail.com>
2023-02-28 18:36:15 +0100

    finish version

--- reactForms1/src/App.jsx
+++ reactForms1/src/App.jsx
@@ -1,30 +1,20 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import hex2rgb from "./components/converter";
+import hex2rgb from "./js/converter";
 
 const Feedback = () => {
   const [color, setColor] = useState("");
-  let timerID = null;
 
   const change = (el) => {
-    if (timerID) {
-      clearTimeout(timerID);
-      timerID = null;
+    if (el.target.value.length === 7 && el.target.value[0] === "#") {
+      setColor(hex2rgb(el.target.value));
+    } else {
+      setColor("Ошибка");
     }
-
-    timerID = setTimeout(() => {
-      if (el.target.value.length === 7 && el.target.value[0] === "#") {
-        setColor(hex2rgb(el.target.value));
-        const color2 = hex2rgb(el.target.value);
-        el.nativeEvent.srcElement.form.style.backgroundColor = color2;
-      } else {
-        setColor("Ошибка");
-      }
-    }, 2000);
   };
 
   return (
-    <form className="form" onChange={change}>
+    <form className="form" onChange={change} style={{backgroundColor: `${color}`}}>
       <input className="input-color" placeholder="Введите HEX цвет"></input>
       <div className="answer">{color}</div>
     </form>
